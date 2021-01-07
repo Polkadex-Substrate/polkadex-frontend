761416b8d16b74a358e7fcae9b2e92a50b7afe57
RudarDaman <damanrudar@gmail.com>
2021-01-07 19:06:41 +0530

    Twitter Implementation with backend

--- src/templates/twitter/index.tsx
+++ src/templates/twitter/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
+import { webSocket } from '../../components/dashboard/CustomChart/api/stream'
 import * as S from './styles'
 import SocialIcons from '../../components/general/SocialIcons'
 import BackgroundPattern from '../../components/general/BackgroundPattern'
@@ -8,6 +10,19 @@ import BackgroundPattern from '../../components/general/BackgroundPattern'
 export default function Twitter({ account }) {
   const [buttonClick, setButtonClick] = useState(false);
   const tweet = 'Hey @polkadex it\'s ' + account?.address + ', give me some @polkadex testnet tokens for https://testnet.polkadex.trade';
+  const router = useRouter()
+
+  const webSocketInstance = webSocket;
+
+  const listeningTweet = () => {
+    setButtonClick(true);
+    webSocketInstance.emit('new-token-request', account?.address);
+    webSocketInstance.on('tweet-success', (data) => {
+      console.log(data);
+      setButtonClick(false);
+      router.push('/dashboard');
+    })
+  }
 
   return (
     <S.ModalWrapper>
@@ -59,9 +74,9 @@ export default function Twitter({ account }) {
             </S.BackButton>
             <S.TwitterButton target="_blank"
               // disabled={accountId === ''}
-                             onClick={() => setButtonClick(true)}
-                             href={'https://twitter.com/intent/tweet?text=' + tweet.toString() + '&&hashtags=Polkadex'}
-                             data-size="large">
+               onClick={() => listeningTweet()}
+               href={'https://twitter.com/intent/tweet?text=' + tweet.toString() + '&&hashtags=Polkadex'}
+               data-size="large">
               <S.TwitterImage src="img/twitter.png" alt="Twitter"/>
               Tweet
             </S.TwitterButton>
